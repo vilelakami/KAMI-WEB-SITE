7e945c4b72963f903619242fb902759a67ef21bc
vilelakami <vilelakamy18@gmail.com>
2026-02-13 19:11:21 -0300

    ajustando o nav responsivo e md

--- KAMI-WEBSITES/website-kami/src/assets/components/Header.jsx
+++ KAMI-WEBSITES/website-kami/src/assets/components/Header.jsx
@@ -7,19 +7,14 @@ function Header(){
 
         return(
             <nav className="fixed top-0 left-0 w-full bg-transparent p-6 z-50 text-white">
-                <div className="flex items-start justify-center">
+                <div className="flex-col items-center justify-start">
                 {/* botão hamburger: para celular */}
-                <div className="md:hidden">
-                    <button onClick={() => setIsOpen(!isOpen)} class="text-2xl flex w-auto h-auto relative z-[60]">{isOpen ? '✕' : '☰'}</button>
+                <div className="w-auto h-auto md:hidden">
+                    <button onClick={() => setIsOpen(!isOpen)} class="text-2xl relative z-[60]">{isOpen ? '✕' : '☰'}</button>
                 </div>
-                <ul className={`${isOpen ? 'block' : 'hidden'} md:flex flex-col md:flex-row 
-                    absolute md:static 
-                    top-20 left-6 md:top-0 md:left-0
-                    w-48 md:w-auto 
-                    bg-black/80 md:bg-transparent 
-                    items-start md:items-center 
-                    space-y-6 md:space-y-0 md:space-x-10 
-                    p-6 md:p-0 rounded-lg md:rounded-none`}>
+                <ul className={`${isOpen ? 'block' : 'hidden'} md:flex md:flex-row 
+                    w-auto h-auto md:w-auto space-y-2 md:space-y-0 md:space-x-15 
+                    p-4 md:p-0 md:justify-center`}>
                     <li><a href="#">about</a></li>
                     <li><a href="#">contact</a></li>
                     <li><a href="#">portfolio</a></li>
